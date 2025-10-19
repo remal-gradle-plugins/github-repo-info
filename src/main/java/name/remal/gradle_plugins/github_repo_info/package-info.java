b073b16bc6a9c4f5a34e924eb6c6bc80e50b5ed4
Semyon Levin <levin.semen@gmail.com>
2025-10-20 03:10:55 +0400

    test

--- src/main/java/name/remal/gradle_plugins/github_repo_info/package-info.java
+++ src/main/java/name/remal/gradle_plugins/github_repo_info/package-info.java
@@ -1,4 +1,32 @@
+@Value.Style(
+    defaults = @Value.Immutable(
+    ),
+    visibility = ImplementationVisibility.SAME,
+    builderVisibility = BuilderVisibility.PUBLIC,
+    jdkOnly = true,
+    get = {"is*", "get*"},
+    optionalAcceptNullable = true,
+    privateNoargConstructor = true,
+    typeBuilder = "*Builder",
+    typeInnerBuilder = "BaseBuilder",
+    allowedClasspathAnnotations = {
+        org.immutables.value.Generated.class,
+        Nullable.class,
+        javax.annotation.Nullable.class,
+        Immutable.class,
+        ThreadSafe.class,
+        NotThreadSafe.class,
+    },
+    depluralize = true
+)
 @NullMarked
 package name.remal.gradle_plugins.github_repo_info;
 
+import javax.annotation.concurrent.Immutable;
+import javax.annotation.concurrent.NotThreadSafe;
+import javax.annotation.concurrent.ThreadSafe;
+import org.immutables.value.Value;
+import org.immutables.value.Value.Style.BuilderVisibility;
+import org.immutables.value.Value.Style.ImplementationVisibility;
 import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
